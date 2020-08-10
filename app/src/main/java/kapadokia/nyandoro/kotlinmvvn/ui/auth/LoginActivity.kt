cd3fcf6d2b6474fc43b1ef0f7b0b6d70a5d9a4c5
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-08-11 01:31:51 +0300

    coding to add .

--- app/src/main/java/kapadokia/nyandoro/kotlinmvvn/ui/auth/LoginActivity.kt
+++ app/src/main/java/kapadokia/nyandoro/kotlinmvvn/ui/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package kapadokia.nyandoro.kotlinmvvn
+package kapadokia.nyandoro.kotlinmvvn.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kapadokia.nyandoro.kotlinmvvn.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
